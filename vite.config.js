b5f19c6cfc7fd3c6876ea5d584aa6f3b0a50bf01
dolani25 <akinmarvelous2030@gmail.com>
2025-04-05 14:18:35 +0100

    2nd update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-})
+  resolve: {
+    alias: {
+      // Ensure proper resolution of react-router-dom
+      'react-router-dom': require.resolve('react-router-dom'),
+    },
+  },
+});
